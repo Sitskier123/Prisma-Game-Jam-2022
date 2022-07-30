0eeefcbfec24e9ce7561210ea97ba2fbe737c9ca
Sitskier123 <sitskier123@gmail.com>
2022-07-29 21:43:57 -0600

    First Script

---
+++ PrismaGameJam(Kyle)/Assets/Scriptable Objects/PlayerData.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "ScriptableObjects/PlayerData", order = 1)]
+public class PlayerData : ScriptableObject
+{
+    //Physics
+    [Header("Gravity")]
+    public float gravityScale;
+    public float fallGravityMult;
+    public float quickFallGravityMult;
+
+
+}
