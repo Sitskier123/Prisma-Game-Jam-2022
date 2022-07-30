393829b2a8394aba7a0a8dc1ebda14902ca08037
Sitskier123 <sitskier123@gmail.com>
2022-07-29 22:42:21 -0600

    Fuck u

--- PrismaGameJam(Kyle)/Assets/Scriptable Objects/PlayerData.cs
+++ PrismaGameJam(Kyle)/Assets/Scriptable Objects/PlayerData.cs
@@ -14,4 +14,32 @@ public class PlayerData : ScriptableObject
     [Header("Drag")]
     public float amountDrag;
     public float amountFriction;
+
+    [Header("Other Physics")]
+    [Range(0, 0.5f)] public float coyoteTime;
+
+    //Ground
+    [Header("Run")]
+    public float runMaxSpeed;
+    public float runAccel;
+    public float runDeccel;
+    [Range(0,1)] public float accelInAir;
+    [Range(0,1)] public float deccelInAir;
+    [Space(5)]
+    [Range(0.5f, 2f)] public float accelPower;
+    [Range(0.5f, 2f)] public float stopPower;
+    [Range(0.5f, 2f)] public float turnPower;
+
+    //Jump
+    [Header("Jump")]
+    public float jumpForce;
+    [Range(0, 1)] public float jumpCutMultiplier;
+    [Space(10)]
+    [Range(0, 0.5f)] public float jumpBufferTime;
+
+    //Other
+    [Header("Other Settings")]
+    public bool doKeepRunMomentum;
+    public bool doTurnOnWallJump;
+
 }
