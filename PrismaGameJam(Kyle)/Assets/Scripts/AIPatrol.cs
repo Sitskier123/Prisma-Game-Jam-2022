f31d4b15ba0c16e76c993d45fdb718ae92092a8a
Caden McVey <59580833+caden602@users.noreply.github.com>
2022-07-29 23:28:08 -0600

    Enemy Health Bars
    Added Enemy Health Bars

--- PrismaGameJam(Kyle)/Assets/Scripts/AIPatrol.cs
+++ PrismaGameJam(Kyle)/Assets/Scripts/AIPatrol.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class AIPatrol : MonoBehaviour
 {
+    public float Hitpoints;
+    public float MaxHitpoints = 5;
+    public HealthbarBehavior Healthbar;
+
     public float walkSpeed;
     public Rigidbody2D rb;
     public Transform groundCheckPos;
@@ -18,6 +22,8 @@ public class AIPatrol : MonoBehaviour
     void Start()
     {
         mustPatrol = true;
+        Hitpoints = MaxHitpoints;
+        Healthbar.SetHealth(Hitpoints, MaxHitpoints);
     }
 
     // Update is called once per frame
@@ -53,5 +59,15 @@ public class AIPatrol : MonoBehaviour
         transform.localScale = new Vector2(transform.localScale.x * -1, transform.localScale.y);
         walkSpeed *= -1;
         mustPatrol = true;
+    }
+
+    public void TakeHit(float damage)
+    {
+        Hitpoints -= damage;
+
+        if (Hitpoints <= 0)
+        {
+            Destroy(gameObject);
+        }
     }
 }
