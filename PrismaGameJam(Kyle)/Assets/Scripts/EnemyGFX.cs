ea6500b5e4330d8e763202d5c19f1c10a65626db
Caden McVey <59580833+caden602@users.noreply.github.com>
2022-07-30 08:42:22 -0600

    Trying to make the dam Bat sleep

--- PrismaGameJam(Kyle)/Assets/Scripts/EnemyGFX.cs
+++ PrismaGameJam(Kyle)/Assets/Scripts/EnemyGFX.cs
@@ -9,13 +9,13 @@ public class EnemyGFX : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
+    { 
         if(aiPath.desiredVelocity.x >= 0.01f)
         {
             transform.localScale = new Vector3(-1f, 1f, 1f);
         } else if (aiPath.desiredVelocity.x <= -0.01f)
         {
-            transform.localScale = new Vector3(1f, 1f, 1f);
+            transform.localScale = new Vector3(-1f, 1f, 1f);
         }
     }
 }
